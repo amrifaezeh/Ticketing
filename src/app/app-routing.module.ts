5d962e014e782ed78ebc687e7ec74d6c90705bf7
Faezeh Amri <amrifaezeh@gmail.com>
2019-09-16 20:34:06 +0800

    create basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { BookingComponent } from './ticketing/booking/booking.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'booking', component: BookingComponent },
+  { path: '', redirectTo: '/booking', pathMatch: 'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
